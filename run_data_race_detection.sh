5ae2669d35acb6361ffce2ee1b8d1dfab0fd60eb
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2022-08-22 15:04:44 +0200

    correction in start script

--- run_data_race_detection.sh
+++ run_data_race_detection.sh
@@ -1,4 +1,4 @@
 ORIGINAL_DIR=$PWD
 cd /home/jovyan/discopop
 
-timeout 80s python -m discopop_validation --path=${ORIGINAL_DIR} --dep-file=out_dep.txt --ll-file=out_dp_inst.ll --verbose=true --dp-build-path=/home/jovyan/discopop/build/  --data-race-output=${ORIGINAL_DIR}/data_races.txt --dp-profiling-executable=${ORIGINAL_DIR}/run_dp_maker.sh
+timeout 80s python -m discopop_validation --path=${ORIGINAL_DIR} --ll-file=out_dp_inst.ll --verbose=true --dp-build-path=/home/jovyan/discopop/build/  --data-race-output=${ORIGINAL_DIR}/data_races.txt --dp-profiling-executable=${ORIGINAL_DIR}/run_dp_maker.sh --pet-dump-file=pet_dump.json
