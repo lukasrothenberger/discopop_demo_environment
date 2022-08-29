3cd02c0027c96c5a9764ef2a14ae4184cd495837
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2022-08-29 10:22:58 +0200

    modified sample_code.cpp

--- sample_code.cpp
+++ sample_code.cpp
@@ -6,12 +6,8 @@
 int main(){
     int x = 42;
     int y = 42;
-    #pragma omp parallel
-    {
-    #pragma omp for
     for(int i = 0; i < 100; i++){
         x = x + i;
-    }
     }
     printf("The answer is: %d\n", x);
 }
